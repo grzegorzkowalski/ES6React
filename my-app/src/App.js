bccf6a911f8a1c3ca9bbf32b7e93ee38c1e104b0
grzegorzkowalski <grzegorzkowalski@yahoo.com>
2021-12-17 09:33:07 +0100

    pass even

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,20 +6,22 @@ import GrandchildComponent from "./components/GrandchildComponent";
 import './App.css';
 import HelloWorld from "./components/HelloWorld";
 import Buttons from "./components/Buttons";
+import ButtonCounter from "./components/ButtonCounter";
 
 function App() {
   return (
     <div style={{margin: "20px"}}>
-        <Search />
-        <Hello name="Grzegorz" surname="Kowalski" />
-        <Hello name="John" surname="Rambo" age={40} />
-        <ParentComponent>
-            <ChildComponent>
-                <GrandchildComponent />
-            </ChildComponent>
-        </ParentComponent>
-        <HelloWorld />
-        <Buttons />
+        {/*<Search />*/}
+        {/*<Hello name="Grzegorz" surname="Kowalski" />*/}
+        {/*<Hello name="John" surname="Rambo" age={40} />*/}
+        {/*<ParentComponent>*/}
+        {/*    <ChildComponent>*/}
+        {/*        <GrandchildComponent />*/}
+        {/*    </ChildComponent>*/}
+        {/*</ParentComponent>*/}
+        {/*<HelloWorld />*/}
+        {/*<Buttons />*/}
+        <ButtonCounter />
 
     </div>
   );
